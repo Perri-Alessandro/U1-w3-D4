01a6cc339232cd45a24edb122bcd37b8708ba362
Alessandro <perrialessandro93@gmail.com>
2023-12-07 16:56:15 +0100

    devo ancora fare un sacco di pratica

--- js/index.js
+++ js/index.js
@@ -11,3 +11,22 @@ const creaTabellone = function () {
   }
 };
 creaTabellone();
+
+// ESTRAGGO UN NUMERO CASUALE DA 1 A 76 E LO EVIDENZIO NEL TABELLONE
+const estraiN = function () {
+  const min = 1;
+  const max = 76;
+  let numeroRandom;
+  do {
+    numeroRandom = Math.floor(Math.random() * (max - min + 1)) + min;
+    // ESTRAGGO UN NUOVO NUMERO, FINCHè NON è STATO ESTRATTO IN PRECEDENZA
+  } while (numeriEstratti.includes(numeroRandom));
+  // ORA AGGIUNGO QUESTO NUMERO AD ARRAY CREATO INIZIALMENTE
+  numeriEstratti.push(numeroRandom);
+  // ORA DEVO MOSTRARE IL NUMERO ESTRATTO NELLA CELLA
+  const cella = document.getElementById("cella" + numeroRandom);
+  cella.classList.add("estratti");
+  cella.textContent = numeroRandom;
+  document.getElementById("numeroEstratto").textContent =
+    "ULTIMO NUMERO ESTRATTO: " + numeroRandom;
+};
